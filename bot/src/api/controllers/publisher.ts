1fa551c6b10b29f4ccb39346eb1ce591c11092f9
Diego Maroto <diego.diemg@gmail.com>
2024-07-12 11:17:22 +0200

    temp secret removed, as well as revoked

--- bot/src/api/controllers/publisher.ts
+++ bot/src/api/controllers/publisher.ts
@@ -3,7 +3,6 @@
 import { HttpAbstractEvent, HttpFactory } from '../../http/http.lambda';
 import { APIGatewayV2Event } from '../../http/typings/apigatewayv2.event';
 import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
-const tempSecret = '51fcf02a5c2a43094a879d5dddcf9cd6f9b86c2f477df3008187607fd8ee48ab';
 // This is your AWS Lambda handler
 export const handler = async (event: APIGatewayV2Event): Promise<HttpAbstractEvent> => {
   // Initialize AWS SQS with the new AWS SDK v3
@@ -14,9 +13,6 @@ export const handler = async (event: APIGatewayV2Event): Promise<HttpAbstractEve
     // Parse the message from the API Gateway event
     const body = JSON.parse(event.body); // Assuming the message to publish is in the request body
     const bodySecret = body.secret;
-    if (bodySecret !== tempSecret || true) {
-      return HttpFactory.createUnauthorizedResponse('Unauthorized');
-    }
     const message = JSON.stringify({ message: body.message, timestamp: Date.now() });
     // Publish message to SQS topic using AWS SDK v3
     const command = new SendMessageCommand({
